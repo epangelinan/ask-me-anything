4158aa2d71c241745a2973c39f382daced1d8851
Esvethlynna Pangelinan <esveth@aol.com>
2017-08-11 16:52:45 -0700

    add functionality to delete the answer.  Also add functionality to delete the corresponding answers from the database after deleting a question

--- app/routes/question.js
+++ app/routes/question.js
@@ -25,8 +25,18 @@ export default Ember.Route.extend({
       this.transitionTo('question', question);
     },
 
+    destroyAnswer(answer) {
+      answer.destroyRecord();
+      this.transitionTo('index');
+    },
+
     destroyQuestion(question) {
-      question.destroyRecord();
+      var answer_deletions = question.get('answers').map(function(answer) {
+        return answer.destroyRecord();
+      });
+      Ember.RSVP.all(answer_deletions).then(function() {
+        return question.destroyRecord();
+      });
       this.transitionTo('index');
     }
   }
